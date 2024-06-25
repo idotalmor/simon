7e775ccf425beee17cc22b85da26dccbbf4b81d2
ido talmor <talmorid@gmail.com>
2024-06-25 13:02:25 +0300

    introduce points to game state, added play function with first game iteration, still wip

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -11,7 +11,7 @@ import { it } from "@jest/globals";
 
 // Note: test renderer must be required after react-native.
 import renderer from "react-test-renderer";
-import { useSimon, standByObj, GameState } from "../src/hooks/useSimon.ts";
+import { useSimon, standByObj, GameState, SimonState } from "../src/hooks/useSimon.ts";
 import { act, renderHook } from "@testing-library/react-native";
 
 describe("simon game", () => {
@@ -19,9 +19,27 @@ describe("simon game", () => {
     const { result } = renderHook(() => useSimon());
     expect(result.current.state).toEqual(standByObj);
     act(() => result.current.newGame());
-    expect(result.current.state).toMatchObject({ gameState: GameState.Playing, userStepIndex: 0 });
+    expect(result.current.state).toMatchObject({ gameState: GameState.Playing, userStepIndex: 0, points:0 });
     expect(result.current.state.sequence).toHaveLength(1);
   });
+
+  it("play 1 successful step",()=>{
+    const { result } = renderHook(() => useSimon());
+
+    act(() => result.current.newGame()); //create new game
+
+    //play 1 step
+    const { sequence } = result.current.state;
+    let playResult: boolean | undefined;
+    act(() => {playResult = result.current.play(sequence[0]);});
+    expect(playResult).toBe(true);
+
+    //check game status
+    const gameStatus: Partial<SimonState> = {gameState: GameState.Playing, userStepIndex: 0,points: 1};
+    expect(result.current.state).toMatchObject(gameStatus);
+    expect(result.current.state.sequence).toHaveLength(2);//check new sequence
+  });
+
 });
 
 it("renders correctly", () => {
--- src/hooks/useSimon.ts
+++ src/hooks/useSimon.ts
@@ -4,35 +4,64 @@ enum GameState {Standby, Playing, End}
 
 type SimonState = {
   gameState: GameState;
-  sequence?: number[];
-  userStepIndex?: number
+  sequence: number[];
+  userStepIndex: number,
+  points: number
 }
 
 const standByObj: SimonState = {
-  gameState: GameState.Standby
+  gameState: GameState.Standby,
+  sequence: [],
+  userStepIndex: 0,
+  points: 0
 };
 
 type UseSimonReturnType = {
   state: SimonState;
   newGame: () => void;
+  play: (selected: number) => boolean
 };
 
 const useSimon = (): UseSimonReturnType => {
 
   const [state, setState] = useState<SimonState>(standByObj);
 
-
   const newGame = () => {
     const newStep = getRandomNumber(0, 3);
     const newGameObj: SimonState = {
       gameState: GameState.Playing,
       sequence: [newStep],
-      userStepIndex: 0
+      userStepIndex: 0,
+      points: 0
     };
     setState(newGameObj);
   };
 
-  return { state, newGame };
+  const play = (selected: number): boolean => {
+    if (state.gameState !== GameState.Playing) return false;
+    setState(state => {
+      if (selected === state.sequence[state.userStepIndex]) {//if step correct
+        if (state.userStepIndex === state.sequence.length - 1) {//was last in sequence
+          return {
+            ...state,
+            points: state.points + 1,
+            sequence: [...state.sequence, getRandomNumber(0, 3)],
+            userStepIndex: 0
+          };
+        } else { //just a successful step
+          //increment user position
+
+        }
+      } else {//if user get the step wrong
+        //change game status to end
+      }
+
+      return state;
+    });
+    return true;
+  };
+
+  return { state, newGame, play };
 };
 
 const getRandomNumber = (min: number, max: number): number => {
@@ -42,3 +71,4 @@ const getRandomNumber = (min: number, max: number): number => {
 };
 
 export { useSimon, standByObj, GameState };
+export type { SimonState };
