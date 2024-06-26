7dc601054fef34ac1a28416a83451d4cd9ad2e63
ido talmor <talmorid@gmail.com>
2024-06-27 01:59:17 +0300

    add provider to app tree to connect store to react app

--- App.tsx
+++ App.tsx
@@ -5,34 +5,41 @@
  * @format
  */
 
-import React from 'react';
+import React from "react";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import SimonGameScreen from './src/features/simonGame/screens/SimonGameScreen.tsx';
+import SimonGameScreen from "./src/features/simonGame/screens/SimonGameScreen.tsx";
 import ResultsScreen from "./src/features/results/screens/ResultsScreen.tsx";
 import { RootStackParamList, Screens } from "./src/navigation.ts";
+import { Provider } from "react-redux";
+import store, { persistor } from "./src/store/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 function App(): React.JSX.Element {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={Screens.SimonGame}>
-        <Stack.Screen
-          name={Screens.SimonGame}
-          component={SimonGameScreen}
-          options={{ title: 'Simon Game' }}
-        />
-        <Stack.Screen
-          name={Screens.ResultScreen}
-          component={ResultsScreen}
-          options={{ title: 'Results' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName={Screens.SimonGame}>
+            <Stack.Screen
+              name={Screens.SimonGame}
+              component={SimonGameScreen}
+              options={{ title: "Simon Game" }}
+            />
+            <Stack.Screen
+              name={Screens.ResultScreen}
+              component={ResultsScreen}
+              options={{ title: "Results" }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 
 }
